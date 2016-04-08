9e1cdd9896a0a8925bad4c868123b6c9789d0d16
alisha-agrawal <alisha.agrawal@ucsf.edu>
2016-04-08 12:57:05 -0700

    change from epicid to msbid

--- R/api_search_by_msbid.R
+++ R/api_search_by_msbid.R
@@ -1,27 +1,27 @@
-#' Search MS Bioscreen for a specific EPICID
+#' Search MS Bioscreen for a specific MSB ID
 #'
-#' \code{api_search_by_epicid} returns the bioscreen entries associated with the specified \code{epicid}.
+#' \code{api_search_by_msbid} returns the bioscreen entries associated with the specified \code{msbid}.
 #'
 #' @inheritParams api_check
-#' @param epicid EPIC identifier
+#' @param msbid msbioscreen identifier
 #'
 #' @seealso \code{\link{api_do_action}}, \code{\link{api_create}}, \code{\link{api_update}},
 #' \code{\link{api_delete}}, \code{\link{api_get}}, \code{\link{api_search_by_updated_at}}
 #' @export
 
-api_search_by_epicid = function(epicid = 1, endpoint = "subjects",
+api_search_by_msbid = function(msbid = 1, endpoint = "subjects",
                                 base_url = "https://msbioscreen-uat.herokuapp.com/api/v1",
                                 token = get_token(), verbose_b = TRUE){
 
   if (verbose_b) {
-    cat(sprintf("Searching %s for epicid = %s...", endpoint, epicid))
+    cat(sprintf("Searching %s for msbid = %s...", endpoint, msbid))
   }
 
   # getting url of app
   url = paste(base_url, endpoint, "search",sep = "/")
 
   # searching data
-  search_by_list = list(list(query = list(epicid = epicid)))
+  search_by_list = list(list(query = list(epicid = msbid)))
   response_data = api_do_action(action = POST, url = url, token = token, json_body_data = to_json_non_array(search_by_list))
 
   # check status and return data frame if ok
@@ -31,7 +31,7 @@ api_search_by_epicid = function(epicid = 1, endpoint = "subjects",
   if(success) {
     return(response_to_data_frame(response_data))
   } else {
-    stop(sprintf("SUFL_API_ERROR: Wrong http status (%d) when searching %s for epicid = %s", response_data$status_code, endpoint, epicid))
+    stop(sprintf("SUFL_API_ERROR: Wrong http status (%d) when searching %s for msbid = %s", response_data$status_code, endpoint, msbid))
   }
 
 }
